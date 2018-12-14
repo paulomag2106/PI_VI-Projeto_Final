4ed3285882d1ce660118bfd9f3d4cac04ded0714
Danilo Augusto Mative <danilo.amative@gmail.com>
2018-12-13 23:44:24 -0200

    position ok. fixing group distri

--- model.c
+++ model.c
@@ -1,9 +1,10 @@
 #include "model.h"
 
-#define MODELSCALE 13.2f
-#define MODELDIST 20.f
-#define MODEL_Z 25.f
-#define GROUPDIST 50.f
+#define MODELSCALE 7.f
+#define MODELDIST_Y 3.f
+#define MODELDIST_X 1.f
+#define MODEL_Z 5.f
+#define GROUPDIST 2.f
 
 
 v4 standardColor = (v4) {1.f,1.f,1.f,1.f};
@@ -68,42 +69,42 @@ void createInitialEnvironment() {
             preyObj.color = standardColor;
             wolfObj.color = standardColor;
             
-            wolfGroup.objectArray[m] = wolfObj;
-            preyGroup.objectArray[m] = preyObj;
-            
             v3 preyPosition = newV3(sites[i].x + GROUPDIST, sites[i].y, MODEL_Z);
             v3 wolfPosition = newV3(sites[i].x - GROUPDIST, sites[i].y, MODEL_Z);
             
-            float xChange = MODELDIST, yChange = MODELDIST;
+            float xChange = MODELDIST_X, yChange = MODELDIST_Y;
             
             switch (m) {
                 case 0:
                     xChange = 0.f;
                     yChange = 0.f;
                 case 1:
-                    xChange = MODELDIST;
-                    yChange = -MODELDIST;
+                    xChange = -MODELDIST_X;
+                    yChange = MODELDIST_Y;
                 case 2:
-                    xChange = MODELDIST;
-                    yChange = MODELDIST;
+                    xChange = -MODELDIST_X;
+                    yChange = -MODELDIST_Y;
                 case 3:
-                    xChange = -MODELDIST;
-                    yChange = -MODELDIST;
+                    xChange = -2.f * MODELDIST_X;
+                    yChange = 0.f;
                 case 4:
-                    xChange = -MODELDIST;
-                    yChange = MODELDIST;
+                    xChange = -2.f * MODELDIST_X;
+                    yChange = 2.f * MODELDIST_Y;
+                case 5:
+                    xChange = -2.f * MODELDIST_X;
+                    yChange = -2.f * MODELDIST_Y;
                 case 6:
-                    xChange = 0.f;
-                    yChange = MODELDIST * 2.f;
+                    xChange = -3.f * MODELDIST_X;
+                    yChange = MODELDIST_Y;
                 case 7:
-                    xChange = 0.f;
-                    yChange = MODELDIST * -2.f;
+                    xChange = -3.f * MODELDIST_X;
+                    yChange = -MODELDIST_Y;
                 case 8:
-                    xChange = MODELDIST * 2.f;
-                    yChange = 0.f;
+                    xChange = -4.f * MODELDIST_X;
+                    yChange = MODELDIST_Y;
                 case 9:
-                    xChange = MODELDIST * -2.f;
-                    yChange = 0.f;
+                    xChange = -4.f * MODELDIST_X;
+                    yChange = -MODELDIST_Y;
             }
             
             wolfPosition.x += xChange;
@@ -112,14 +113,24 @@ void createInitialEnvironment() {
             preyPosition.x += -xChange;
             preyPosition.y += yChange;
             
+            wolfPosition.x -= (TWIDTH/2.f);
+            wolfPosition.y -= (TWIDTH/2.f);
+            
+            preyPosition.x -= (TWIDTH/2.f);
+            preyPosition.y -= (TWIDTH/2.f);
+            
+            
             preyObj.position = preyPosition;
             wolfObj.position = wolfPosition;
             
-            scaleObjTo(&preyObj, newV3(MODELSCALE*preyDensity, MODELSCALE*preyDensity, MODELSCALE*preyDensity));
-            scaleObjTo(&wolfObj, newV3(MODELSCALE*wolfDensity, MODELSCALE*wolfDensity, MODELSCALE*wolfDensity));
+            scaleObjTo(&preyObj, newV3(MODELSCALE, MODELSCALE, MODELSCALE));
+            scaleObjTo(&wolfObj, newV3(MODELSCALE, MODELSCALE, MODELSCALE));
             
             rotateObjBy(&preyObj, newV3(0, 0, 1), M_PI / 2.f);
             rotateObjBy(&wolfObj, newV3(0, 0, 1), -M_PI / 2.f);
+            
+            wolfGroup.objectArray[m] = wolfObj;
+            preyGroup.objectArray[m] = preyObj;
         }
         
         
